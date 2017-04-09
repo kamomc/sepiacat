f65ebe4ff7c1e11fe2e872c65cd7b7635b5b9971
kamomc <kamoc.dev@gmail.com>
2017-04-09 18:09:41 +0900

    検索フォームを追加

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,8 +1,19 @@
 class MapController < ApplicationController
   def show
+    @q = Spot.search(name_or_comment_cont: search_param)
     @markers = []
-    for spot in Spot.all do
+    for spot in @q.result do
       @markers.push(spot.marker)
+    end
+  end
+
+
+  private
+  def search_param
+    if params[:q] == nil then
+      []
+    else
+      params.require(:q).permit(:name_or_comment_cont)[:name_or_comment_cont]
     end
   end
 end
