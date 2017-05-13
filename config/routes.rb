01903f1ea98663b8b65ca24800690fdfe283e66e
kamomc <kamoc.dev@gmail.com>
2017-05-13 11:33:37 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'map' => 'map#show'
 
   resources :spots
-  resources :users
+  resources :users, :only => [:index, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match ':controller(/:action(/:id))', via: [:get, :post, :patch]
   root to: 'map#show'
